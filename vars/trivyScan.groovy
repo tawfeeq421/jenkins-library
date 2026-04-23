b9c09752fc65a58181a548def15984a2a0c646b3
TawwfeeqAhmed <engr.tawfeeq@gmail.com>
2026-04-23 15:48:46 +0530

    modified trivy scan stage

--- vars/trivyScan.groovy
+++ vars/trivyScan.groovy
@@ -4,7 +4,7 @@ def call(String type, String image = ""){
         trivy fs . \
         --severity HIGH,CRITICAL \
         --format table \
-        -- no-progress \
+        --no-progress \
         > trivyfs-report.txt
         '''
     }
