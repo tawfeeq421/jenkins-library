c6f26900b51f6cc67c551eb75a2dd6494d3aa0bd
TawwfeeqAhmed <engr.tawfeeq@gmail.com>
2026-04-23 14:58:29 +0530

    added trivy scan stage

---
+++ vars/trivyScan.groovy
@@ -0,0 +1,23 @@
+def call(String type, String image = ""){
+    if (type == "fs"){
+        sh '''
+        trivy fs . \
+        --severity HIGH,CRITICAL \
+        --format table \
+        -- no-progress \
+        > trivyfs-report.txt
+        '''
+    }
+    else if (type == "image"){
+        sh """
+        trivy image ${DOCKER_IMAGE}:${DOCKER_TAG} \
+        --severity HOGH,CRITICAL \
+        --format table \
+        --no-progress \
+        > trivy-image-report.txt
+        """
+    }
+    else {
+        error "Invalid type. use 'fs' or 'image' "
+    }
+}
